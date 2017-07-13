5fa0fb2296be6163461a8ee74efd3d73ea298785
Hokuto Abe <hokuto@test.jp>
2017-07-13 13:27:12 +0900

    edit eba_controller

--- app/controllers/eba_controller.rb
+++ app/controllers/eba_controller.rb
@@ -8,8 +8,6 @@ class EbaController < ApplicationController
   end
   def search
      @column = ["title","user_id","classname","uploaded_time"]
-    if request.post?
      @questions = EbaQuestion.order(params[:sort]).where(['title LIKE ?', "%#{params[:search]}%"])
-    end
   end
 end
